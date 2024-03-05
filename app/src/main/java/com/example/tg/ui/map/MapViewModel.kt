ee12accfbec18968f85d72d85873bea8037ac262
George Hewett <96452665+GDL109R@users.noreply.github.com>
2024-03-05 14:49:02 +0000

    Start app with map page

--- app/src/main/java/com/example/tg/ui/map/MapViewModel.kt
+++ app/src/main/java/com/example/tg/ui/map/MapViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class MapViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is Map Fragment"
+        value = "This is the Map Fragment"
     }
     val text: LiveData<String> = _text
 }
