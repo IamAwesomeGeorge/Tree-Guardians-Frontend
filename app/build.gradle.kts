c6d903d01f79087e693402443a875ba92938815f
milena1208 <127429970+milena1208@users.noreply.github.com>
2024-03-13 15:50:35 +0000

    implementing Retrofit code for API

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,7 +66,20 @@ dependencies {
     implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
     implementation("androidx.legacy:legacy-support-v4:1.0.0")
     implementation("com.google.android.gms:play-services-maps:18.2.0")
+
+
+    // Retrofit implementation for API access
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation ("com.google.code.gson:gson:2.10.1")
+    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
+
+
+
     testImplementation("junit:junit:4.13.2")
+
+
+
+
 
 //    androidTestImplementation("androidx.test.ext:junit:1.1.5")
 //    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
---
+++ app/src/main/java/models/TreeModel.kt
@@ -0,0 +1,22 @@
+package models
+
+import kotlin.properties.Delegates
+
+public class TreeModel {
+    private var id: Int by Delegates.notNull<Int>()
+
+    private var creation_date: String by Delegates.notNull<String>()
+    private var id_user: Int by Delegates.notNull<Int>()
+    private var species: String by Delegates.notNull<String>()
+    private var latitude: Double by Delegates.notNull<Double>()
+    private var longitude: Double by Delegates.notNull<Double>()
+    private var health_status: String by Delegates.notNull<String>()
+    private var circumference: Double by Delegates.notNull<Double>()
+    private var planted: String by Delegates.notNull<String>()
+    private var height: Int by Delegates.notNull<Int>()
+    private var is_deleted: Boolean by Delegates.notNull<Boolean>()
+
+
+
+
+}
